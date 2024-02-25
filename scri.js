f12c50e67370afbbed5974d879915c8c50bcec30
kartik joon <kartikjoon171@gmail.com>
2024-02-25 15:39:26 +0530

    change colors

---
+++ scri.js
@@ -0,0 +1,38 @@
+
+/*let c= document.getElementsByClassName("container")
+let b= document.getElementsByClassName("icon")
+let a= document.getElementsByClassName("ans")
+console.log(c)
+
+for(let i=0; i<c.length; i++){
+    c[i].addEventListener("click",function (){
+        a[i].style.transition="0.2s"
+       let res= c[i].classList.toggle("active")
+       
+       // b[i].style.rotate=" -180deg"
+        if(res === true){
+            b[i].style.rotate="135deg"
+            b[i].style.transition="0.2s"
+            b[i].style.color="dodgerblue"
+            
+        }
+        
+        else if(res ===false){
+            b[i].style.rotate="0deg"
+            b[i].style.transition="0.2s"
+            b[i].style.color="black"
+        }
+    })
+
+}*/
+
+let a= document.getElementsByClassName("ques")
+
+console.log(a)
+//
+//let c= document.querySelectorAll(".ans")
+//console.log(m)
+//console.log(j)
+//console.log(c)
+
+
